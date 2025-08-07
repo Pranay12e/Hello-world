f7e3174e5c482265d121d589067f22de388570c1
Pranay <pranaykarri412@gmail.com>
2025-08-07 09:28:19 +0530

    Initial commit - Hello World Maven project

---
+++ src/test/java/com/example/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testDummy() {
+        assertTrue(true);  // dummy test
+    }
+}
+
